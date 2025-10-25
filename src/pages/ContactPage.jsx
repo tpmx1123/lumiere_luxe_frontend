ac86464c231e2d21f1997925138e8acbdd4ed238
tpmx <info@tpmx.in>
2025-10-25 20:38:56 +0530

    Final Project

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -28,7 +28,7 @@ const ContactPage = () => {
   };
 
   const handleWhatsAppClick = () => {
-    window.open('https://wa.me/919876543210', '_blank');
+    window.open('https://wa.me/917075447437', '_blank');
   };
 
   return (
@@ -69,12 +69,12 @@ const ContactPage = () => {
                     </div>
                     <div className="contact-text">
                       <h3>Visit Us</h3>
-                      <p>123 Luxury Lane<br />Madhapur, Hyderabad<br />Telangana 500081</p>
+                      <p>Lumiere Luxe Salon, <br />Madhapur, Hyderabad<br />Telangana 500081</p>
                       <span className="click-hint">Click to open in Maps</span>
                     </div>
-                  </div>
+                  </div> 
 
-                  <div className="contact-method" onClick={() => handlePhoneClick('+919876543210')}>
+                  <div className="contact-method" onClick={() => handlePhoneClick('+91 92345 69999')}>
                     <div className="contact-icon">
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" fill="currentColor"/>
@@ -82,12 +82,12 @@ const ContactPage = () => {
                     </div>
                     <div className="contact-text">
                       <h3>Call Us</h3>
-                      <p>+91 98765 43210<br />+91 87654 32109</p>
+                      <p>+91 92345 69999<br />+91 947965 9999</p>
                       <span className="click-hint">Click to call</span>
                     </div>
                   </div>
 
-                  <div className="contact-method" onClick={() => handleEmailClick('info@lumiereluxe.com')}>
+                  <div className="contact-method" onClick={() => handleEmailClick('lumiereluxe0030@gmail.com')}>
                     <div className="contact-icon">
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" fill="currentColor"/>
@@ -95,7 +95,7 @@ const ContactPage = () => {
                     </div>
                     <div className="contact-text">
                       <h3>Email Us</h3>
-                      <p>info@lumiereluxe.com<br />bookings@lumiereluxe.com</p>
+                      <p>lumiereluxe0030@gmail.com</p>
                       <span className="click-hint">Click to email</span>
                     </div>
                   </div>
@@ -108,7 +108,7 @@ const ContactPage = () => {
                     </div>
                     <div className="contact-text">
                       <h3>WhatsApp</h3>
-                      <p>+91 98765 43210<br />Quick booking & inquiries</p>
+                      <p>+91 70754 47437<br />Quick booking & inquiries</p>
                       <span className="click-hint">Click to chat</span>
                     </div>
                   </div>
