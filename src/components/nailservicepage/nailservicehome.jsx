38585d4df2c5e0679f989b3e0718d4154d99267c
tpmx <info@tpmx.in>
2025-10-27 16:18:22 +0530

    Final Project

--- src/components/nailservicepage/nailservicehome.jsx
+++ src/components/nailservicepage/nailservicehome.jsx
@@ -13,7 +13,7 @@ const nailservice = () => {
       <img 
         src={vectorBrush} 
         alt="Vector brush decoration" 
-        className="absolute top-[-10px] -left-[45px] w-[170px] h-[190px] z-[1] opacity-90 object-contain pointer-events-none hidden lg:block" 
+        className="absolute top-[-10px] -right-[px] w-[170px] h-[190px] z-[3] opacity-90 object-contain pointer-events-none block" 
       />
       
       {/* Flowers SVG as background */}
@@ -23,17 +23,17 @@ const nailservice = () => {
         className="absolute top-0 left-0 right-0 bottom-0 w-full h-full object-cover z-[1] opacity-100 pointer-events-none lg:opacity-100 md:opacity-70 sm:opacity-50"
       />
       
-      <div className="py-5 pb-[200px] relative z-[2] min-h-screen flex flex-col md:pb-[150px] sm:pb-[100px]">
+      <div className="py-5 pb-[450px] relative z-[2] min-h-screen flex flex-col md:pb-[350px] sm:pb-[300px]">
         <div className="max-w-[1200px] mx-auto px-5 relative">
           <h1 className="font-playfair text-salon-gold text-[5.2rem] m-0 mb-[5px] text-center relative font-medium tracking-[0.5px] leading-[1.2] lg:text-[5.2rem] md:text-[4rem] sm:text-[2.8rem] sm:mb-4">
             Nail Services
           </h1>
           
-          <p className="font-inter text-salon-dark-brown text-[1.2rem] text-center max-w-[1200px] mx-auto mt-[5px] mb-5 leading-[1.3] font-normal lg:text-[1.2rem] md:text-[1.1rem] sm:text-base sm:max-w-[90%]">
+          <p className="font-inter text-salon-dark-brown text-[1.2rem] text-center max-w-[1200px] mx-auto mt-[5px] mb-[20px] leading-[1.3] font-normal lg:text-[1.2rem] md:text-[1.1rem] sm:text-base sm:max-w-[90%] md:mb-[15px] sm:mb-[10px]">
             At Lumiere Luxe, we believe nails are more than beauty. They are an expression. Each detail and shade is crafted to reflect your mood, personality, and rhythm. Our experts combine global artistry with thoughtful care to create looks that are refined, lasting, and uniquely yours.
           </p>
          
-          <div className="absolute bottom-[-375px] left-0 right-0 text-center z-[3] pointer-events-none h-[40%] flex items-end justify-center md:bottom-[-300px] md:h-[30%] sm:bottom-[-200px] sm:h-[30%]">
+          <div className="absolute bottom-[-450px] left-0 right-0 text-center z-[3] pointer-events-none h-[40%] flex items-end justify-center md:bottom-[-350px] md:h-[30%] sm:bottom-[-300px] sm:h-[30%]">
             <img 
               src={NailImage} 
               alt="Nail care" 
--- src/components/skinandbody/skinandbodyhome.jsx
+++ src/components/skinandbody/skinandbodyhome.jsx
@@ -8,23 +8,23 @@ import skinandbodyimg from '../../assets/skinandbodyimg.png';
 
 const SkinAndBodyHome = () => {
   return (
-    <div className="bg-salon-light-beige min-h-screen py-2.5 px-5 relative overflow-hidden flex flex-col">
+    <div className="bg-salon-light-beige min-h-screen py-2.5 px-5 relative overflow-hidden pb-[500px] md:pb-[400px] sm:pb-[300px]">
       {/* Vector brush SVG at top left */}
       <img 
         src={vectorBrush} 
         alt="Vector brush decoration" 
-        className="absolute top-[-10px] -left-[45px] w-[180px] h-auto z-10 opacity-90 max-w-full hidden lg:block" 
+        className="absolute top-[-10px] -left-[45px] w-[180px] h-auto z-10 opacity-90 max-w-full block" 
       />
       
       {/* Flowers SVG as background */}
       <img 
         src={flowers} 
         alt="Flowers background" 
-        className="absolute top-[0px] left-0 w-full h-full object-cover z-[1] opacity-90 mix-blend-multiply max-w-full hidden lg:block"
+        className="absolute top-[0px] left-0 w-full h-full object-cover z-[1] opacity-90 mix-blend-multiply max-w-full block"
       />
       
       <div className="text-center m-0 p-0 relative z-[2] mt-0">
-        <div className="max-w-[1200px] mx-auto px-5">
+        <div className="max-w-[1200px] mx-auto px-5 relative">
           <h1 className="font-playfair text-salon-gold text-[4.5rem] m-0 mb-2.5 font-light tracking-[1.5px] leading-[1.1] relative z-[2] lg:text-[4.5rem] md:text-[3.5rem] sm:text-[2.8rem]">
             Skin & Body
           </h1>
@@ -38,11 +38,13 @@ const SkinAndBodyHome = () => {
             blends advanced skincare science with the art of touch - ensuring results that feel as good as they look.
           </p>
           
-          <img 
-            src={skinandbodyimg} 
-            alt="Skin and body service tools" 
-            className="max-w-full w-[1000px] h-auto mt-[80px] mx-auto flex rounded-[10px] z-[5] pb-0 mb-10 md:w-[600px] md:mt-[60px] md:mb-8 sm:w-[400px] sm:mt-[50px] sm:mb-6" 
-          />
+          <div className="absolute bottom-[-450px] left-0 right-0 text-center z-[3] pointer-events-none h-[40%] flex items-end justify-center md:bottom-[-350px] md:h-[30%] sm:bottom-[-250px] sm:h-[30%]">
+            <img 
+              src={skinandbodyimg} 
+              alt="Skin and body service tools" 
+              className="max-w-[1200px] w-[90%] h-auto  object-contain pointer-events-none select-none rounded-[10px] md:max-w-[800px] md:w-[85%] sm:max-w-[600px] sm:w-[80%]" 
+            />
+          </div>
         </div>
       </div>
     </div>
